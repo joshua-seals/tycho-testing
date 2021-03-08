b58cc6103cf3c3a79de877989c177130b7c4b2f9
Matthew Brightman <brightman.matthew@gmail.com>
2021-03-08 13:26:52 -0500

    update to start system test

--- tycho/test/test_actions.py
+++ tycho/test/test_actions.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('/home/mattb523/tycho')
+
 import os
 import json
 import logging
@@ -9,6 +12,9 @@ from tycho.test.lib import system
 from tycho.test.lib import system_request
 from unittest import mock
 
+from tycho.actions import StartSystemResource
+from tycho.actions import TychoResource
+
 logger = logging.getLogger (__name__)
 
 def test_start_system_resource(mocker, system_request, client, request):
@@ -18,39 +24,39 @@ def test_start_system_resource(mocker, system_request, client, request):
         "result": {
             "name": "jupyter-ds-caa94baea8a849d89e427bd78cad17eb",
             "sid": "caa94baea8a849d89e427bd78cad17eb",
-            "containers": {
-                "jupyter-datascience": {
-                    "ip_address": "127.0.0.1",
-                    "port-1": 32661
-                }
-            }
         },
         "message": "Started system jupyter-ds-caa94baea8a849d89e427bd78cad17eb"
     }
 
     # Making a system request, storing in tycho_system
-    # What does it need to be asserted to match up to???
-    tycho_system = system_request()
-    result = tycho_system['result']
-    jupyter = result['containers']['jupyter-datascience']
+    tycho_system = StartSystemResource().post(system_request)
 
-    assert tycho_system.status == 'success'
-    assert tycho_system.name == result['name']
-    assert tycho_system.identifier == result['sid']
-    assert tycho_system.services[0].ip_address == jupyter['ip_address']
-    assert tycho_system.services[0].port == jupyter['port-1']
-    assert tycho_system.message == response['message']
+    result = response["result"]
+    
+    assert tycho_system['status'] == response['status']
+    assert tycho_system['name'] == result['name']
+    assert tycho_system['sid'] == result['sid']
+    assert tycho_system['message'] == response['message']
 
 def test_delete_system_resource(mocker, system_request, client, request):
     print(f"{request.node.name}")
-    response = {
-        "status": "success",
-        "result": {
-            #not sure what to put here
-        }
-    }
+    # response = {
+    #     "status": "success",
+    #     "result": {
+    #         #result
+    #     }
+    #     "message": "Deleted system ..."
+    # }
 
-    tycho_system = 
+    # tycho_delete = DeleteSystemResource().post(system_request)
+
+    # result = response['result']
+
+    # assert tycho_delete['status'] == response['status']
+    # assert tycho_delete['name'] == result['name']
+    # assert tycho_delete['sid'] == result['sid']
+    # assert tycho_delete['message'] == response['message']
+
 
 def test_status_system_resource(mocker, system_request, client, request):
     print(f"{request.node.name}")
